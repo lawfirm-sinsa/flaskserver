8952095bdf02dcdc69dcc3db91c61623754ec9e0
lawfirm-sinsa <lawfirm.sinsa@gmail.com>
2019-10-07 16:46:09 +0900

    minor change

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
 
+@app.route('/lawyer')
+def lawyer():
+    return render_template('lawyer.html')
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80, debug=True)
 
 
 
